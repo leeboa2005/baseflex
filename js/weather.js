bd526788a1e15ff12b83d88ef9ae24fde2c5b68e
leeboa2005 <71476841+leeboa2005@users.noreply.github.com>
2024-04-17 14:36:48 +0900

    Delete js directory

--- js/weather.js
+++
@@ -1,8 +0,0 @@
-// 날씨 데이터 출력
-window.addEventListener('load', function () {
-    fetch("http://spartacodingclub.shop/sparta_api/weather/seoul").then(res => res.json()).then(data => {
-        let nowWeather = data['temp']
-        const weather = document.querySelector('#weather');
-        weather.innerHTML = `${nowWeather}° 입니다.`
-    })
-})
